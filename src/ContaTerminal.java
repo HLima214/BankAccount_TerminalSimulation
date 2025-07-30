eea1b5540c2aea11daf5b0f1df1c15519357c639
Henrique Lima <riquelima00@gmail.com>
2025-07-30 20:26:09 -0300

    Add comments

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -3,12 +3,13 @@ import java.util.Scanner;
 public class ContaTerminal {
     public static void main(String[] args) {
 
+        //Criando a classe Scanner para ler os dados do usuário
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Bem-vindo ao JavaBank!");
 
         System.out.println("Digite o número da conta: ");
-        int numeroConta = scanner.nextInt();
+        int numeroConta = scanner.nextInt(); // Lê o número da conta como um inteiro
 
         System.out.println("Digite o número da agência: ");
         String numeroAgencia = scanner.next();
@@ -20,7 +21,7 @@ public class ContaTerminal {
         System.out.println("Digite o saldo da conta: ");
         double saldoConta = scanner.nextDouble();
 
-
+        // Exibindo os dados da conta
         System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + numeroAgencia + " conta " + numeroConta + " e seu saldo " + saldoConta + " já está disponível para saque!");
 
         
